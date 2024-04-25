21e20ef3fc3596dc505158a9238ff769148b6ba0
Nazarii Stakhovskyi <Nazar Stakhovskyi>
2024-04-25 20:41:20 +0300

    add more experiments, fixup readme.md

--- README.md
+++ README.md
@@ -2,26 +2,63 @@
 
 ## Results:
 
-The 1st *50* items from the list that is to be sorted: 
+The first **50** items from the list that is to be sorted: 
 
 [
     9000000.0, 0.0, 9818181.818181818, 9727272.727272728, 1272727.2727272727, 4090909.090909091, 9272727.272727273, 6636363.636363637, 3454545.4545454546, 7909090.909090909, 6818181.818181818, 4181818.181818182, 5818181.818181818, 4272727.2727272725, 8363636.363636364, 3818181.8181818184, 6909090.909090909, 8636363.636363637, 2727272.7272727275, 4909090.909090909
 ]
 
 
-The time it took to sort the list of 111 random numbers is:
-0.0025147999986074865 using sorting via merging;
-0.0003904000041075051 using sorting via insertions;
-3.5599994589574635e-05 using sorting via the native python sorted() function.
+### The time it took to sort the list of <ins>111</ins> random numbers is:
+
+- 0.003 using sorting via `merging`;
+- 0.0004 using sorting via `insertions`;
+- 3.5599994589574635e-05 using sorting via the native python `sorted()` function.
+
+### The time it took to sort the list of <ins>555</ins> random numbers is:
+
+- 0.016 using sorting via `merging`;
+- 0.006 using sorting via `insertions`;
+- 0.0002 using sorting via the native python `sorted()` function.
 
 
-The time it took to sort the list of 1111 random numbers is:
-0.03416539999307133 using sorting via merging;
-0.02349220000905916 using sorting via insertions;
-0.0003040000010514632 using sorting via the native python sorted() function.
+### The time it took to sort the list of <ins>1111</ins> random numbers is:
+
+- 0.03 using sorting via `merging`;
+- 0.02 using sorting via `insertions`;
+- 0.0003 using sorting via the native python `sorted()` function.
+
+### The time it took to sort the list of <ins>5555</ins> random numbers is:
+
+- 0.21 using sorting via `merging`;
+- 0.57 using sorting via `insertions`;
+- 0.0016 using sorting via the native python `sorted()` function.
 
 
-The time it took to sort the list of 11111 random numbers is:
-0.4325315000023693 using sorting via merging;
-2.1726441999926465 using sorting via insertions;
-0.003282499994384125 using sorting via the native python sorted() function.
+### The time it took to sort the list of <ins>11111</ins> random numbers is:
+
+- 0.4 using sorting via `merging`;
+- 2.2 using sorting via `insertions`;
+- 0.003 using sorting via the native python `sorted()` function.
+
+
+### The time it took to sort the list of <ins>55555</ins> random numbers is:
+
+- 2.6 using sorting via `merging`;
+- 55.8 using sorting via `insertions`;
+- 0.02 using sorting via the native python `sorted()` function.
+
+### The time it took to sort the list of <ins>111111</ins> random numbers is:
+
+- 5.7 using sorting via `merging`;
+- 221.2 using sorting via `insertions`;
+- 0.04 using sorting via the native python `sorted()` function.
+
+
+### Visualize the results:
+
+![alt text](image.png)
+
+## Results:
+
+As can be seen from the experiments and the graph representation of those, the inbuilt function `sorted()` is by far (approx **6000** times faster) exceeds sorting via `merging`, and at least **150** times faster than sorting via `insertions`.
--- task_1.py
+++ task_1.py
@@ -3,6 +3,7 @@ from functools import partial
 
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 import timeit
 
 def make_test_data(num_items = 100):
@@ -10,57 +11,60 @@ def make_test_data(num_items = 100):
     random.shuffle(test_data)
     return test_data
 
-test_data = make_test_data(111)
-print(f"\nThe 1st 50 items from the list that is to be sorted: {test_data[:20]}.\n", sep = "\n")
+def run_tests(num_items):
+    test_data = make_test_data(num_items)
+    part_merge = partial(merger, arr = test_data)
+    part_insert = partial(insertion_sort, arr = test_data)
+    native_sort = partial(sorted, test_data)
 
-part_merge = partial(merger, arr = test_data)
-part_insert = partial(insertion_sort, arr = test_data)
-native_sort = partial(sorted, test_data)
+    merge_sort_time = timeit.timeit(stmt = part_merge, number = 20)
+    insert_sort_time = timeit.timeit(stmt = part_insert, number = 20)
+    native_sorted_time = timeit.timeit(stmt = native_sort, number = 20)
 
-merge_sort_time = timeit.timeit(stmt = part_merge, number = 20)
-insert_sort_time = timeit.timeit(stmt = part_insert, number = 20)
-native_sorted_time = timeit.timeit(stmt = native_sort, number = 20)
+    print(
+        f"\nThe time it took to sort the list of {len(test_data)} random numbers is:",
+        f"{merge_sort_time} using sorting via merging;",
+        f"{insert_sort_time} using sorting via insertions;",
+        f"{native_sorted_time} using sorting via the native python sorted() function.\n",
+        sep = "\n"
+        )
+    
+    return merge_sort_time, insert_sort_time, native_sorted_time
 
-print(
-    "\nThe time it took to sort the list of 111 random numbers is:",
-    f"{merge_sort_time} using sorting via merging;",
-    f"{insert_sort_time} using sorting via insertions;",
-    f"{native_sorted_time} using sorting via the native python sorted() function.\n",
-    sep = "\n"
+
+low_data = tuple(run_tests(111))
+low_mid_data = tuple(run_tests(555))
+mid_data = tuple(run_tests(1111))
+mid_large_data = tuple(run_tests(5555))
+large_data = tuple(run_tests(11111))
+large_very_large_data = tuple(run_tests(55555))
+very_large_data = tuple(run_tests(111111))
+
+mr, = plt.plot(
+    [
+        low_data[0], low_mid_data[0], mid_data[0], mid_large_data[0], large_data[0], large_very_large_data[0], very_large_data[0]
+    ],
+    color = "b",
+    alpha = 0.7,
+    label = "Merge"
     )
-
-test_data = make_test_data(1111)
-
-part_merge = partial(merger, arr = test_data)
-part_insert = partial(insertion_sort, arr = test_data)
-native_sort = partial(sorted, test_data)
-
-merge_sort_time = timeit.timeit(stmt = part_merge, number = 20)
-insert_sort_time = timeit.timeit(stmt = part_insert, number = 20)
-native_sorted_time = timeit.timeit(stmt = native_sort, number = 20)
-
-print(
-    "\nThe time it took to sort the list of 1111 random numbers is:",
-    f"{merge_sort_time} using sorting via merging;",
-    f"{insert_sort_time} using sorting via insertions;",
-    f"{native_sorted_time} using sorting via the native python sorted() function.\n",
-    sep = "\n"
+ins, = plt.plot(
+    [
+        low_data[1], low_mid_data[1], mid_data[1], mid_large_data[1], large_data[1], large_very_large_data[1], very_large_data[1]
+    ],
+    color = "r",
+    alpha = 0.7,
+    label = "Insert"
     )
-
-test_data = make_test_data(11111)
-
-part_merge = partial(merger, arr = test_data)
-part_insert = partial(insertion_sort, arr = test_data)
-native_sort = partial(sorted, test_data)
-
-merge_sort_time = timeit.timeit(stmt = part_merge, number = 20)
-insert_sort_time = timeit.timeit(stmt = part_insert, number = 20)
-native_sorted_time = timeit.timeit(stmt = native_sort, number = 20)
-
-print(
-    "\nThe time it took to sort the list of 11111 random numbers is:",
-    f"{merge_sort_time} using sorting via merging;",
-    f"{insert_sort_time} using sorting via insertions;",
-    f"{native_sorted_time} using sorting via the native python sorted() function.\n",
-    sep = "\n"
+srt, = plt.plot(
+    [
+        low_data[2], low_mid_data[2], mid_data[2], mid_large_data[2], large_data[2], large_very_large_data[2], very_large_data[2]
+    ],
+    color = "g",
+    alpha = 0.7,
+    label = "sorted()"
     )
+plt.xlabel('Num experiments')
+plt.ylabel('Seconds')
+plt.legend(handles = [mr, ins, srt])
+plt.show()
